b2865eb15fd3d06ce0d2f6fff4753835532a6647
khanh nguyen <khanh.nguyen.contact@gmail.com>
2022-10-25 07:14:43 +0800

    deleted:    make/.gitignore 	deleted:    make/Makefile 	deleted:    make/hello.cpp 	deleted:    make/include/loader.h 	deleted:    make/print.cpp 	deleted:    make/src/loader.cpp 	deleted:    make/text/kernel.txt

--- make/print.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include"loader.h"
-
-int main() {
-    auto content = loader::load();
-    std::cout << content << std::endl;
-    return 0;
-}
--- make/src/loader.cpp
+++
@@ -1,7 +0,0 @@
-#include"loader.h"
-#include"text/kernel.txt.h"
-namespace loader {
-std::string load() {
-    return std::string(reinterpret_cast<const char*>(text_kernel_txt), text_kernel_txt_len);
-}
-} // namespace loader
